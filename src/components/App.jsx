a56eae8efc7bfe468bf7345fb2d7bfbd02b54c34
Garima Singh <81284494+garimasinghgryffindor@users.noreply.github.com>
2023-03-05 12:26:29 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import emojipedia from "../emojipedia.js";
+import Card from "./Card";
+
+function createCard(emojipedia) {
+  return (
+    <Card
+      key={emojipedia.id}
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      meaning={emojipedia.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+    </div>
+  );
+}
+
+export default App;
